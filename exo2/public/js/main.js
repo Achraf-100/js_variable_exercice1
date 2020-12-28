10ab9bccccb3bc9e49ed14b27eb0b830ba2779b3
Achraf <hardy1348@live.fr>
2020-12-28 12:51:23 +0100

    fin

---
+++ exo2/public/js/main.js
@@ -0,0 +1,43 @@
+let partieUne = "J'aime les tartines et j'en mange ";
+console.log(partieUne);
+
+let jour = " 4 par jours";
+console.log(jour);
+
+let Uno = "je suis cool";
+let Secondo = " et cool";
+console.log(Uno + Secondo);
+
+console.log(partieUne + jour );
+
+let Cinq = 5;
+console.log(5);
+
+let trenteCinq = 35;
+console.log(35);
+
+console.log(trenteCinq + Cinq);
+
+console.log(partieUne + Cinq);
+
+
+
+
+let salut = "Salut tout le monde";
+console.log(salut);
+let aurevoir = "Au revoir";
+console.log(aurevoir);
+salut = aurevoir;
+console.log(salut);
+
+
+
+
+let quatre = 4;
+let six = "6"
+quatre > six
+quatre < six
+quatre == six
+quatre != six
+quatre === six
+quatre !== six
